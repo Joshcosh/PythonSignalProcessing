88d005c29d0fe2014bb36f3b889cc2bbccf97b51
Uri Shilo <u.shilo@gmail.com>
2019-05-03 20:58:02 +0300

    present fft better from 0 to Fs/2

--- main.py
+++ main.py
@@ -16,33 +16,36 @@ Fs, reference = wf.read('./audio/ref_samples.wav')
 reference = np.double(reference) * normalizationFactor
 reference = reference[0:reference.size - 1]
 
-h = sig.firwin(256, 6000, 40, fs=Fs)
+h = sig.firwin(256, 0.5)
+f, H = sig.freqz(h, 1, 1000, fs=16000)
+plt.plot(f, 20*np.log10(np.abs(H)))
 
-y = sig.lfilter(h,1,reference)
-# x = np.arange(y.size) / Fs
-# plt.plot(x,reference)
-# plt.plot(x,y)
-# plt.legend(['Unfiltered', 'Filtered'])
-# plt.show()
-# plt.savefig('filtering of reference with fir filter')
+y = sig.lfilter(h, 1, reference)
+x = np.arange(y.size) / Fs
 
+plt.plot(x, reference)
+plt.plot(x, y)
+plt.legend(['Unfiltered', 'Filtered'])
+plt.show()
+plt.savefig('./graphs/filtering of reference with fir filter')
 
 referencefftabs = np.abs(fft(reference))
+referencefftabs = referencefftabs[0:np.int(referencefftabs.size/2)]
 yfftabs = np.abs(fft(y))
+yfftabs = yfftabs[0:int(yfftabs.size/2)]
 resolution = np.double(referencefftabs.size)
-step = Fs/resolution
-x = np.arange(-8000, 8000, step)
+step = Fs/(2.0*resolution)
+x = np.arange(0, 8000, step)
 
 plt.plot(x, referencefftabs)
 plt.plot(x, yfftabs)
 plt.legend(['Unfiltered', 'Filtered'])
 plt.show()
-plt.savefig('fft filtering of reference with fir filter')
+plt.savefig('./graphs/fft filtering of reference with fir filter')
 
 y = y/normalizationFactor
 y = np.int16(y)
 
-wf.write('filteredReference.wav', Fs, y)
-
+wf.write('./audioout/filteredReference.wav', Fs, y)
 
 print('Boom')
