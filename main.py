ccf421c7db0572ea137cbf604d7d2cbbcf4f4f84
Uri Shilo <u.shilo@gmail.com>
2019-05-04 15:59:32 +0300

    Plot the section and the original song together
     for section6

--- main.py
+++ main.py
@@ -16,23 +16,23 @@ Fs, reference = wf.read('./audio/ref_samples.wav')
 reference = np.double(reference) * normalizationFactor
 reference = reference[0:reference.size - 1]
 
-Fs, section = wf.read('./audio/section2.wav')
+Fs, section = wf.read('./audio/section6.wav')
 section = np.double(section) * normalizationFactor
 section = section[0:section.size - 1]
 
 
 h = sig.firwin(255, 0.1, pass_zero=False)
 f, H = sig.freqz(h, 1, 1000, fs=16000)
-plt.plot(f, 20*np.log10(np.abs(H)))
+# plt.plot(f, 20*np.log10(np.abs(H)))
 
 y = sig.lfilter(h, 1, reference)
 x = np.arange(y.size) / Fs
 
-plt.plot(x, reference)
-plt.plot(x, y)
-plt.legend(['Unfiltered', 'Filtered'])
-plt.show()
-plt.savefig('./graphs/filtering of reference with fir filter')
+# plt.plot(x, reference)
+# plt.plot(x, y)
+# plt.legend(['Unfiltered', 'Filtered'])
+# plt.show()
+# plt.savefig('./graphs/filtering of reference with fir filter')
 
 referencefftabs = np.abs(fft(reference))
 referencefftabs = referencefftabs[0:np.int(referencefftabs.size/2)]
@@ -42,14 +42,14 @@ resolution = np.double(referencefftabs.size)
 step = Fs/(2.0*resolution)
 x = np.arange(0, 8000, step)
 
-plt.plot(x, referencefftabs)
-plt.plot(x, yfftabs)
-plt.legend(['Unfiltered', 'Filtered'])
-plt.show()
-plt.savefig('./graphs/fft filtering of reference with fir filter')
+# plt.plot(x, referencefftabs)
+# plt.plot(x, yfftabs)
+# plt.legend(['Unfiltered', 'Filtered'])
+# plt.show()
+# plt.savefig('./graphs/fft filtering of reference with fir filter')
 
-y = y/normalizationFactor
-y = np.int16(y)
+# y = y/normalizationFactor
+# y = np.int16(y)
 
 
 indexOfHighestCorrelation = np.argmax(np.correlate(y, section))
@@ -60,7 +60,13 @@ SecondOfHighestCorrelation = (indexOfHighestCorrelation / np.double(Fs)) % 60.0
 startIndex = indexOfHighestCorrelation
 endIndex = startIndex + section.size
 
-sectionInReference = y[startIndex:endIndex]
+sectionInReference = reference[startIndex:endIndex]
+
+plt.plot(sectionInReference)
+plt.plot(section)
+plt.legend(['sectionInReference', 'section'])
+plt.show()
+plt.savefig('./graphs/section found comparison')
 
 wf.write('./audioout/section2InReference.wav', Fs, sectionInReference)
 
