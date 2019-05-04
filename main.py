48cc4f667c7680ca7ede8125636c5915e2a5a55b
Uri Shilo <u.shilo@gmail.com>
2019-05-04 21:01:06 +0300

    Cleanup

--- main.py
+++ main.py
@@ -4,72 +4,88 @@ import scipy.io.wavfile as wf
 import scipy.signal as sig
 from scipy.fftpack import fft
 
+
+print('Joytunes exercise Uri Masheu')
+
 # add sounddevice later and use the commmand sd.play(data,Fs)
 
-normalizationFactor = 2 ** -15
+normalization_factor = 2 ** -15
 
-Fs, reference = wf.read('./audio/ref_samples.wav')
+frequency, reference = wf.read('./audio/ref_samples.wav')
 
-
-reference = np.double(reference) * normalizationFactor
+reference = np.double(reference) * normalization_factor
 reference = reference[0:reference.size - 1]
 
-Fs, section = wf.read('./audio/section6.wav')
-section = np.double(section) * normalizationFactor
+print('<1>')
+
+frequency, section = wf.read('./audio/section6.wav')
+section = np.double(section) * normalization_factor
 section = section[0:section.size - 1]
 
-
+# todo: rename h to something programmers understand :)
 h = sig.firwin(255, 0.1, pass_zero=False)
+
+# todo: rename H to something programmers understand :)
+# todo: rename f
 f, H = sig.freqz(h, 1, 1000, fs=16000)
-# plt.plot(f, 20*np.log10(np.abs(H)))
+plt.plot(f, 20 * np.log10(np.abs(H)))
 
+print('<2>')
+
+# todo: rename y
 y = sig.lfilter(h, 1, reference)
-x = np.arange(y.size) / Fs
+# todo: rename x
+x = np.arange(y.size) / frequency
+print('<3>')
 
-# plt.plot(x, reference)
-# plt.plot(x, y)
-# plt.legend(['Unfiltered', 'Filtered'])
+plt.plot(x, reference)
+plt.plot(x, y)
+plt.legend(['Unfiltered', 'Filtered'])
 # plt.show()
-# plt.savefig('./graphs/filtering of reference with fir filter')
+plt.savefig('./graphs/filtering of reference with fir filter')
 
-referencefftabs = np.abs(fft(reference))
-referencefftabs = referencefftabs[0:np.int(referencefftabs.size/2)]
-yfftabs = np.abs(fft(y))
-yfftabs = yfftabs[0:int(yfftabs.size/2)]
-resolution = np.double(referencefftabs.size)
-step = Fs/(2.0*resolution)
+reference_fft_abs = np.abs(fft(reference))
+print('Computing fft...')
+reference_fft_abs = reference_fft_abs[0:np.int(reference_fft_abs.size / 2)]
+y_fft_abs = np.abs(fft(y))
+y_fft_abs = y_fft_abs[0:int(y_fft_abs.size / 2)]
+resolution = np.double(reference_fft_abs.size)
+step = frequency / (2.0 * resolution)
 x = np.arange(0, 8000, step)
 
-# plt.plot(x, referencefftabs)
-# plt.plot(x, yfftabs)
-# plt.legend(['Unfiltered', 'Filtered'])
+print('Plotting frequency domain...')
+# todo: Find in plot docs why it gets stuck when running from terminal
+plt.plot(x, reference_fft_abs)
+plt.plot(x, y_fft_abs)
+plt.legend(['Unfiltered', 'Filtered'])
 # plt.show()
-# plt.savefig('./graphs/fft filtering of reference with fir filter')
+plt.savefig('./graphs/fft filtering of reference with fir filter')
 
+y /= normalization_factor
+y = np.int16(y)
 
-# y = y/normalizationFactor
-# y = np.int16(y)
+print('Correlating signal with filtered reference...')
+highest_correlation_index = np.argmax(np.correlate(y, section))
 
+# todo: instead of minute and second, use one variable of type Duration
+highest_correlation_minute = np.floor((highest_correlation_index / np.double(frequency)) / 60.0)
+highest_correlation_second = (highest_correlation_index / np.double(frequency)) % 60.0
 
-indexOfHighestCorrelation = np.argmax(np.correlate(y, section))
+start_index = highest_correlation_index
+end_index = start_index + section.size
 
-MinuteOfHighestCorrelation = np.floor((indexOfHighestCorrelation / np.double(Fs)) / 60.0)
-SecondOfHighestCorrelation = (indexOfHighestCorrelation / np.double(Fs)) % 60.0
+section_in_reference = reference[start_index:end_index]
 
-startIndex = indexOfHighestCorrelation
-endIndex = startIndex + section.size
-
-sectionInReference = reference[startIndex:endIndex]
-
-plt.plot(sectionInReference)
+print('Plotting section and reference in time domain...')
+plt.plot(section_in_reference)
 plt.plot(section)
 plt.legend(['sectionInReference', 'section'])
-plt.show()
+# plt.show()
 plt.savefig('./graphs/section found comparison')
 
-wf.write('./audioout/section2InReference.wav', Fs, sectionInReference)
+print('Saving outputs...')
+wf.write('./audioout/section2InReference.wav', frequency, section_in_reference)
 
+wf.write('./audioout/filteredReference.wav', frequency, y)
 
-wf.write('./audioout/filteredReference.wav', Fs, y)
-
-print('Boom')
+print('Done! :)')
